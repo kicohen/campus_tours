76d8fae19dd2e32db6c2e4eab87c81f0918810e4
kicohen <kicohen@andrew.cmu.edu>
2017-05-01 20:48:43 -0400

    Added urls and views.

--- tours/urls.py
+++ tours/urls.py
@@ -3,4 +3,9 @@ from tours import views as tours_views
 
 urlpatterns = [
 	url(r'^$', tours_views.home, name='home'),
+	url(r'^about$', tours_views.about, name='about'),
+	url(r'^contact$', tours_views.contact, name='contact'),
+	url(r'^destinations$', tours_views.destinations, name='destinations'),
+	url(r'^destination$', tours_views.destination, name='destination'),
+	url(r'^map$', tours_views.map, name='map'),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, _get_queryset, get_list_or_404
 
 # Create your views here.
 def home(request):
-	return None
+	return render(request, 'tours/home.html')
+
+def about(request):
+	return render(request, 'tours/about.html')
+
+def contact(request):
+	return render(request, 'tours/contact.html')
+
+def map(request):
+	return render(request, 'tours/map.html')
+
+def destination(request):
+	return render(request, 'tours/destination.html')
+
+def destinations(request):
+	return render(request, 'tours/list.html')
